32ae7105c60c8dbed33d1cf8cd489468b3b81996
Franko Prifti <frankoprifti@gmail.com>
2019-08-14 23:02:08 +0200

    Added Express

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const puppeteer = require('puppeteer');
-var url = "https://www.google.com/search?hl=en&as_q=who+is+bill+gates&as_epq=&as_oq=&as_eq=&as_nlo=&as_nhi=&lr=lang_en&cr=&as_qdr=all&as_sitesearch=&as_occt=any&safe=images&as_filetype=&as_rights=";
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    await page.screenshot({ path: './screenshoots/page.png' });
+const express = require("express");
+const app = express();
 
-    await browser.close();
-})();
+app.get("/:id", (req, res) => {
+    var param = req.params.id;
+    console.log(param);
+    var url = "https://www.google.com/search?hl=en&as_q=who+is+bill+gates&as_epq=&as_oq=&as_eq=&as_nlo=&as_nhi=&lr=lang_en&cr=&as_qdr=all&as_sitesearch=&as_occt=any&safe=images&as_filetype=&as_rights=";
+    (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url);
+        await page.screenshot({ path: './screenshoots/' + param + '.png' });
+
+        await browser.close();
+    })();
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+        JSON.stringify({
+            data: param
+        })
+    );
+
+})
+app.get("/", (req, res) => {
+    res.send("Data From Google");
+});
+
+app.listen(process.env.PORT || 4000);
